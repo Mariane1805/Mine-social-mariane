225b55a6e6d3d6fb3d49c4f2aa4551582c7cc1e1
Mariane1805 <mariane.8925488@aluno.mg.gov.br>
2026-04-07 18:29:24 +0000

    Atualização de java e html

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
 let likeCount = 0;
-let curtido = false; // flag booleana
+let curtido = false;
+
+let dislikeCount = 0;
+let descurtido = false;
 
 function curtir() {
-
- if(curtido == false){
+  if (curtido == false) {
+    
+    if (descurtido == true) {
+      descurtido = false;
+      dislikeCount--;
+    }
     likeCount++;
     curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
- }else{
+  } else {
+    
     likeCount--;
     curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
- }
+  }
+  atualizarInterface();
+}
 
-  
+function descurtir() {
+  if (descurtido == false) {
+    
+    if (curtido == true) {
+      curtido = false;
+      likeCount--;
+    }
+    dislikeCount++;
+    descurtido = true;
+  } else {
+    
+    dislikeCount--;
+    descurtido = false;
+  }
+  atualizarInterface();
+}
+
+function atualizarInterface() {
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
+document.getElementById("dislikeBtn").addEventListener("click", descurtir);
